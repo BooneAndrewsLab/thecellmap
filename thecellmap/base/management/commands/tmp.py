22db065836237b242facd101001a4f4ef3b5690b
matej <matej@7e663dc3-c743-4763-bac1-1f4d75230683>
2014-01-14 23:19:47 +0000

    - Nicer XLS colours

--- thecellmap/base/management/commands/tmp.py
+++ thecellmap/base/management/commands/tmp.py
@@ -5,29 +5,39 @@ Created on Dec 13, 2013
 '''
 from base.utils import CellMapCommand
 import re
+import xlwt
+from xlwt.Style import colour_map
 
 PAT = re.compile('_T\d+', re.IGNORECASE)
 
 class Command(CellMapCommand):
+    def make_style(self, name):
+        style = xlwt.XFStyle()
+        pattern = xlwt.Pattern()
+        pattern.pattern = xlwt.Pattern.SOLID_PATTERN
+        pattern.pattern_fore_colour = xlwt.Style.colour_map[name]
+        style.pattern = pattern
+        return style
+    
     def handle(self, *args, **options):
-        pairs = {}
-        duplicates = set()
+        wb = xlwt.Workbook()
+        sheet = wb.add_sheet('palette')
+        sheet.paper_size_code = 1
         
-        with open('/home/matej/release_sga_fg_merge_121004_scored_130422_unfiltered.txt') as f:
-            f.readline() # skip header
-            line_num = 1
-            
-            for l in f.readlines():
-                l = l.split() 
-                key = PAT.sub('', l[0]), PAT.sub('', l[2])
-                
-                pairs.setdefault(key, []).append(line_num)
-                if len(pairs[key]) - 1:
-                    duplicates.add(key)
-                
-                line_num += 1
+        xlwt.add_palette_colour("red_stringent", 0x21)
+        wb.set_colour_RGB(0x21, 204, 51, 51)
+        xlwt.add_palette_colour("red_lenient", 0x22)
+        wb.set_colour_RGB(0x22, 255, 153, 153)
+        xlwt.add_palette_colour("green_stringent", 0x23)
+        wb.set_colour_RGB(0x23, 0, 153, 51)
+        xlwt.add_palette_colour("green_lenient", 0x24)
+        wb.set_colour_RGB(0x24, 153, 204, 153)
         
-        for q, a in duplicates:
-            print '\t'.join((q, a, ','.join(map(str, pairs[(q,a)]))))
+        row = 0
+        for k, v in colour_map.iteritems():
+            style = self.make_style(k)
+            sheet.write(row, 0, k, style)
+            sheet.write(row, 1, v, style)
+            row += 1
         
-        
+        wb.save("/home/matej/palette.xls")
