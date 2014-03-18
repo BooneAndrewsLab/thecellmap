28b5f0aae1d907d2b51084ce49cdc3ee449d0fc0
matej <matej@7e663dc3-c743-4763-bac1-1f4d75230683>
2014-03-18 19:39:31 +0000

    - New favicon

--- thecellmap/thecellmap/urls.py
+++ thecellmap/thecellmap/urls.py
@@ -2,6 +2,8 @@
 
 from django.conf.urls import include, patterns, url
 from django.contrib import admin
+
+
 admin.autodiscover()
 
 def bad(request):
@@ -9,9 +11,6 @@ def bad(request):
     1 / 0
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'thecellmap.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^bad/$', bad),
     url(r'', include('base.urls')),
