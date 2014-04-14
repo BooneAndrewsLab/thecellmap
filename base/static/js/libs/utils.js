8835d9c1ff0a823a13d34eca0edf5abe1b5899b7
Matej Usaj <usajusaj@users.noreply.github.com>
2014-04-14 21:35:26 +0000

    - Annotations

--- base/static/js/libs/utils.js
+++ base/static/js/libs/utils.js
@@ -62,6 +62,15 @@ function rgbToHex(rgb) {
     ).toUpperCase();
 }
 
+function hexToRgba(hexStr, a){
+    // note: hexStr should be #rrggbb
+    var hex = parseInt(hexStr.substring(1), 16);
+    var r = (hex & 0xff0000) >> 16;
+    var g = (hex & 0x00ff00) >> 8;
+    var b = hex & 0x0000ff;
+    return [r, g, b, a];
+}
+
 /*
 function componentToHex(c) {
     var hex = c.toString(16);
