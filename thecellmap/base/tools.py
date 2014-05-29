2e9ff08b6d398d258211034ae32631878b601866
matej <matej@7e663dc3-c743-4763-bac1-1f4d75230683>
2014-05-29 15:00:19 +0000

    - Annotations bug fix

--- thecellmap/base/tools.py
+++ thecellmap/base/tools.py
@@ -16,26 +16,29 @@ class AnnotationsForm(Form):
     genes = CharField(widget=Textarea)
 
 def annotations(request):
-    form = AnnotationsForm(request.POST)
+    form = AnnotationsForm()
     
-    if request.POST and form.is_valid():
-        genes = form.cleaned_data['genes'].splitlines()
-        annotation = form.cleaned_data['annotation']
-        response = write_excel_file('annotated_genes_%s.xls' % (datetime.now().strftime('%Y%m%d-%H%M%S'), ))
-        response.add_sheet("Annotated", ['Input label', 'Label', 'ORF', 'Name', 'Annotations'])
+    if request.POST:
+        form = AnnotationsForm(request.POST)
         
-        gmap = gene_map()
-        tmap = {}
-        for tg in Term.genes.through.objects.filter(term__annotation=annotation).select_related('term'):  # @UndefinedVariable
-            tmap.setdefault(tg.gene_id, []).append(tg.term)
-        
-        for g, gene in [(g, gmap.get(g)) for g in genes]:
-            row = [g]
-            if gene:
-                row += [gene.name or gene.orf, gene.orf, gene.name, ';'.join([(hasattr(t, 'name') and t.name or t) for t in tmap.get(gene.id, ['NOT ANNOTATED'])])]
-            response.write_row(row)
-        
-        return response.as_response()
+        if form.is_valid():
+            genes = form.cleaned_data['genes'].splitlines()
+            annotation = form.cleaned_data['annotation']
+            response = write_excel_file('annotated_genes_%s.xls' % (datetime.now().strftime('%Y%m%d-%H%M%S'), ))
+            response.add_sheet("Annotated", ['Input label', 'Label', 'ORF', 'Name', 'Annotations'])
+            
+            gmap = gene_map()
+            tmap = {}
+            for tg in Term.genes.through.objects.filter(term__annotation=annotation).select_related('term'):  # @UndefinedVariable
+                tmap.setdefault(tg.gene_id, []).append(tg.term)
+            
+            for g, gene in [(g, gmap.get(g)) for g in genes]:
+                row = [g]
+                if gene:
+                    row += [gene.name or gene.orf, gene.orf, gene.name, ';'.join([(hasattr(t, 'name') and t.name or t) for t in tmap.get(gene.id, ['NOT ANNOTATED'])])]
+                response.write_row(row)
+            
+            return response.as_response()
     
     return render(request, 'base/annotations.html', {
             'form': form,
