ca571e7fb86c89752744b5c1d811d38f2f254d98
matej <matej@7e663dc3-c743-4763-bac1-1f4d75230683>
2014-06-04 19:36:29 +0000

    - Custom upload with XLS files

--- thecellmap/base/static/js/libs/custom_upload.js
+++ thecellmap/base/static/js/libs/custom_upload.js
@@ -1,5 +1,60 @@
 (function() {
-    var results = null, genes = null, maxid = 0;
+    var results = null, genes = null, maxid = 0, seen;
+    var workbook, processFun, nodeAttrs;
+    
+    function getGeneObj(label, nodes, layout) {
+        var gene, id, obj;
+        
+        if (genes.hasOwnProperty(label)) {
+            gene = genes[label];
+            if (seen.hasOwnProperty(gene.id)) return seen[gene.id];
+            id = gene.id, obj = gene;
+        } else {
+            if (seen.hasOwnProperty(label)) return seen[label];
+            id = ++maxid, obj = {orf: label, id: maxid, label: label};
+            seen[label] = obj;
+        }
+        
+        if (nodeAttrs.hasOwnProperty(label)) {
+            // Add any extra annotation
+            obj.attributes = nodeAttrs[label];
+        };
+        
+        nodes.push(obj);
+        layout.push({x: Math.random() * 2000 - 1000, y: Math.random() * 2000 - 1000, id: id});
+        
+        seen[id] = obj;
+        return obj;
+    }
+    
+    function processXlsWorkbook(workbook, nodes, layout, dataset) {
+        var gene, src, dst, id, obj, h;
+        nodeAttrs = {};
+        seen = {};
+        
+        if (workbook.SheetNames.indexOf('nodes') != -1) {
+            XLS.utils.sheet_to_row_object_array(workbook.Sheets['nodes']).forEach(function(row) {
+                obj = {};
+                for (h in row) {
+                    if (row.hasOwnProperty(h) && h.indexOf('__') != 0 && h != "node") {
+                        obj[h] = row[h];
+                    }
+                }
+                nodeAttrs[row.node] = obj;
+            });
+        }
+        
+        XLS.utils.sheet_to_row_object_array(workbook.Sheets['edges']).forEach(function(row) {
+            obj = {};
+            for (h in row) if (row.hasOwnProperty(h) && h.indexOf('__') != 0) obj[h] = row[h];
+            
+            src = getGeneObj(obj.source, nodes, layout);
+            dst = getGeneObj(obj.target, nodes, layout);
+            
+            if (row.weight == null || row.weight == undefined) row.weight = 0.1;
+            dataset.push({s: src.id, t: dst.id, w: row.weight});
+        });
+    };
     
     $(document).ready(function() {
         $.get(genes_url, function(data) {
@@ -24,101 +79,25 @@
             $("#file-contents").empty();
             $("#generate-btn").addClass('disabled');
         }).change(function() {
-            $(this).parse({
-                before: function(data) {
-                    if (genes == null) {
-                        alert("FOOBAR");
-                    }
-                },
-                error: function(data) {
-                },
-                complete: function(data) {
-                    results = data.results;
-                    var table = $('#data-table').clone().removeAttr('id'), body = table.find('tbody'), row;
-                    var ids = ['source', 'target', 'weight'], numOfField = [0, 0, 0], i = 0;
-                    data.results.fields.forEach(function(field) {
-                        table.find('thead tr').append('<th><div class="btn-group"><a class="btn dropdown-toggle" data-toggle="dropdown" href="#">'
-                                + field + '<span class="caret"></span></a><ul class="dropdown-menu">'
-                                + '<li data-field="source" class=source><a href="#">source</a></li>'
-                                + '<li data-field="target" class=target><a href="#">target</a></li>'
-                                + '<li data-field="weight" class=weight><a href="#">weight</a></li>'
-                                + '</ul></div></th>');
-                        i++;
-                        
-                        for (var j = 0; j < 3; j++) {
-                            if (field == ids[j])
-                                numOfField[j]++;
-                        }
-                    });
-                    
-                    data.results.rows.forEach(function(r) {
-                        row = $('<tr>');
-                        data.results.fields.forEach(function(f) {
-                            row.append('<td>' + r[f] + '</td>');
-                        });
-                        body.append(row);
-                    });
-                    
-                    $("#file-contents").append(table);
-                    
-                    $('.dropdown-menu li').click(function() {
-                        var newField = $(this).closest("li").data("field");
-                        var field = $(this).closest("div").find("a").text().replace("sourcetargetweight","").toLowerCase();
-                        
-                        data.results.rows.forEach(function(r) {
-                            r[newField] = r[field];
-                        });
-                        
-                        $(this).parents('.btn-group').find('.dropdown-toggle').html($(this).text()
-                                + '<span class="caret"></span>');
-                        
-                        for (j = 0; j < 3; j++) {
-                            if (newField == ids[j])
-                                numOfField[j]++;
-                            if (field == ids[j])
-                                numOfField[j]--;
-                        }
-                        
-                        if (numOfField[0] == 1 && numOfField[1] == 1 && numOfField[2] < 2) {
-                            $("#generate-btn").removeClass('disabled');
-                        }
-                        else
-                            $("#generate-btn").addClass('disabled');
-                    });
-                    
-                    if (numOfField[0] == 1 && numOfField[1] == 1) {
-                        $("#generate-btn").removeClass('disabled');
-                    }
-                }
-            });
+            var f = this.files[0];
+            var reader = new FileReader();
+            var name = f.name;
+            reader.onload = function(e) {
+                var data = e.target.result;
+                workbook = XLS.read(data, {type:'binary'});
+                processFun = processXlsWorkbook;
+                
+                processXlsWorkbook(workbook, [], [], []);
+                
+                $("#generate-btn").removeClass('disabled');
+            };
+            reader.readAsBinaryString(f);
         });
         
         $("#generate-btn").click(function() {
-            var gene, seen = {}, src, dst, id, obj;
             var nodes = [], layout = [], dataset = [];
             
-            // nodes.json
-            results.rows.forEach(function(row) {
-                [row.source, row.target].forEach(function(g, idx) {
-                    if (genes.hasOwnProperty(g)) {
-                        gene = genes[g];
-                        if (seen.hasOwnProperty(gene.id)) return;
-                        id = gene.id, obj = gene;
-                    } else {
-                        if (seen.hasOwnProperty(g)) return;
-                        id = ++maxid, obj = {label: g, orf: g, id: maxid};
-                        seen[g] = null;
-                    }
-                    
-                    nodes.push(obj);
-                    seen[id] = null;
-                    layout.push({x: Math.random() * 2000 - 1000, y: Math.random() * 2000 - 1000, id: id});
-                    if (idx == 0) src = id;
-                    else if (idx == 1) dst = id;
-                });
-                if (row.weight == null || row.weight == undefined) row.weight == 0.1;
-                dataset.push({s: src, t: dst, w: row.weight});
-            });
+            processFun(workbook, nodes, layout, dataset);
             
             $.ajax({
                 dataType: 'json', 
