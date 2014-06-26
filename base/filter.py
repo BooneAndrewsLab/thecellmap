e1bb355f7310220d8c700ecadbb316358f439f10
Matej Usaj <usajusaj@users.noreply.github.com>
2014-06-26 14:21:44 +0000

    - Ignore case in annotation tool - Import cleanup

--- base/filter.py
+++ base/filter.py
@@ -1,6 +1,6 @@
 import django_filters
+
 from base.models import Custom
-from django.core.context_processors import request
 
 
 class CustomFilter(django_filters.FilterSet):
--- base/tools.py
+++ base/tools.py
@@ -3,7 +3,7 @@ import hashlib
 import os
 from time import time
 
-from django.core.exceptions import ValidationError
+from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.forms.fields import CharField
 from django.forms.forms import Form
@@ -14,9 +14,8 @@ from django.http.response import HttpResponseBadRequest, HttpResponseRedirect, \
 from django.shortcuts import render
 
 from base.filter import CustomFilter
-from base.models import Annotation, Term, Custom
+from base.models import Annotation, Term, Custom, Strain, Gene
 from base.utils import gene_map, write_excel_file, JsonResponse
-from django.contrib.auth.decorators import login_required
 
 
 ### FORMS ###
@@ -47,15 +46,25 @@ def annotations(request):
             response = write_excel_file('annotated_genes_%s.xls' % (datetime.now().strftime('%Y%m%d-%H%M%S'), ))
             response.add_sheet("Annotated", ['Input label', 'Label', 'ORF', 'Name', 'Annotations'])
             
-            gmap = gene_map()
+            gmap = gene_map(keyfun=lambda x: x.upper())
+            
+            """ Add alleles to the map """
+            for strain in Strain.objects.filter(allele__isnull=False).exclude(allele='').select_related('gene'):
+                gmap[strain.allele.upper()] = strain
+            
             tmap = {}
             for tg in Term.genes.through.objects.filter(term__annotation=annotation).select_related('term'):  # @UndefinedVariable
                 tmap.setdefault(tg.gene_id, []).append(tg.term)
             
-            for g, gene in [(g, gmap.get(g)) for g in genes]:
+            for g, gene in [(g, gmap.get(g.upper())) for g in genes]:
                 row = [g]
-                if gene:
+                
+                if isinstance(gene, Gene):
                     row += [gene.name or gene.orf, gene.orf, gene.name, ';'.join([(hasattr(t, 'name') and t.name or t) for t in tmap.get(gene.id, ['NOT ANNOTATED'])])]
+                elif isinstance(gene, Strain):
+                    strain = gene
+                    gene = gene.gene
+                    row += [strain.allele, gene.orf, gene.name, ';'.join([(hasattr(t, 'name') and t.name or t) for t in tmap.get(gene.id, ['NOT ANNOTATED'])])]
                 response.write_row(row)
             
             return response.as_response()
