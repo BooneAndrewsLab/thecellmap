c91d95bea913fddf42895ff2f564acc30db16687
yizhao <yizhao@7e663dc3-c743-4763-bac1-1f4d75230683>
2015-08-20 18:53:40 +0000

    - better ccbr graph

--- base/static/js/collaboration.js
+++ base/static/js/collaboration.js
@@ -5,11 +5,13 @@ require.config({
         'underscore': 'libs/underscore-min',
         'bootstrap': 'libs/bootstrap.min',
         
+        'three': '//cdnjs.cloudflare.com/ajax/libs/three.js/r71/three',
         'noUISlider': 'libs/nouislider-8.0.1.min',
         'sigma': 'libs/isigma',
         
+        'mouse': 'boone3D/mouseControls',
+        
         'scrollbar': 'libs/jquery.mCustomScrollbar.concat.min',
-        'drawer': 'libs/drawer.min',
         
         'sigma.forcelayout': 'libs/plugins/sigma.fa2',
         'sigma.highlight': 'libs/plugins/sigma.highlight',
@@ -22,9 +24,10 @@ require.config({
         'backbone': ['underscore', 'jquery'],
         'bootstrap': ['jquery'],
         
-        'drawer': ['jquery', 'bootstrap'],
         'noUISlider': ['jquery'],
         'scrollbar': ['jquery'],
+        
+        'mouse' : ['three'],
         
         'sigma.forcelayout': ['sigma'],
         'sigma.highlight': ['sigma'],
