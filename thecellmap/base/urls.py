34db6cb275139e36afddaea41b1157e1320f1ee8
yizhao <yizhao@7e663dc3-c743-4763-bac1-1f4d75230683>
2016-02-05 15:56:51 +0000

    - middleware for checking if user changed password - new UserProfile model to keep track which user updated password

--- thecellmap/base/urls.py
+++ thecellmap/base/urls.py
@@ -1,5 +1,7 @@
 """urlconf for the base application"""
 
+from base.models import Dataset
+import base.password_sync
 from django.conf.urls import url, patterns, include
 from django.contrib.sitemaps import GenericSitemap, Sitemap
 from django.contrib.sitemaps.views import sitemap
@@ -7,7 +9,6 @@ from django.core.urlresolvers import reverse
 from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
 
-from base.models import Dataset
 
 class StaticViewSitemap(Sitemap):
     priority = 0.5
