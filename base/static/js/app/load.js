3f8d17cd59c2970e45e42ce384ff9a008a52e8e0
Matej Usaj <usajusaj@users.noreply.github.com>
2017-01-26 04:05:17 +0000

    - More

--- base/static/js/app/load.js
+++ base/static/js/app/load.js
@@ -1,4 +1,6 @@
 define(['jszip'], function(jszip) {
     //For XLSX library: JSZip library uses browserify which conflict with requirejs and does not exports JSZip as global variable
+    "use strict";
+
     window.JSZip = jszip;
 });
