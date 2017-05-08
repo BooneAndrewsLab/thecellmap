a9d899c73f67ecb190431c67fa77c3f91388abed
matej <matej@7e663dc3-c743-4763-bac1-1f4d75230683>
2017-05-08 17:46:10 +0000

    - Fixed safe download

--- thecellmap/base/views.py
+++ thecellmap/base/views.py
@@ -15,7 +15,8 @@ from django.contrib.auth import login as django_login, logout as django_logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordChangeForm, AuthenticationForm
 from django.db.models.aggregates import Max
-from django.http.response import HttpResponseRedirect, Http404, HttpResponseForbidden, HttpResponseBadRequest
+from django.http.response import HttpResponseRedirect, Http404, HttpResponseForbidden, HttpResponseBadRequest,\
+    FileResponse
 from django.shortcuts import render
 from django.views.decorators.cache import never_cache
 from django.views.decorators.clickjacking import xframe_options_exempt
@@ -28,6 +29,8 @@ from base.utils import print_queries, is_integer, JsonResponse
 import pandas as p
 from sga.safe import Safe
 from pandas.indexes.multi import MultiIndex
+from scipy.stats import hypergeom
+import io
 
 
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:27.0) Gecko/20100101 Firefox/27.0'
@@ -530,22 +533,75 @@ def safe(request, dataset_id=None):
         enrichments = safe.calculate()
     
     if 'dl' in request.POST:
+        with open(dataset.static_path('nodes_inv.pickle')) as fp:
+            nodes_inv = pickle.load(fp)
+        
+        nodes_inv_inv = {}
+        for nid, sids in nodes_inv.iteritems():
+            for sid in sids:
+                nodes_inv_inv[sid] = nid
+        
+        terms = {}
+        all_annotated = 0
+        for term in Term.objects.filter(annotation=dataset.default_annotation_id).prefetch_related('strains'):
+            term_nodes = set()
+            for strain in term.strains.all():
+                if strain.pk in nodes_inv_inv:
+                    term_nodes.add(nodes_inv_inv[strain.pk])
+            
+            terms[term] = term_nodes
+            all_annotated += len(term_nodes)
+        
         node_map = {}
         for n in json.load(open(dataset.static_path('nodes.json')))['nodes']:
             node_map[n['id']] = n
         
         enrichments = enrichments[enrichments.any(axis=1)]
         
-        verbose_index= []
-        for n in enrichments.index:
-            n = node_map[n]
-            verbose_index.append((n['orf'], n['name'], n['alel'], n['label'].lower()))
+        output = io.BytesIO()
+        res_data = p.ExcelWriter(output, engine='xlsxwriter')
         
-        enrichments.index = MultiIndex.from_tuples(verbose_index, names=['ORF', 'Name', 'Allele', 'Label'])
-        enrichments = enrichments.reindex(columns=sorted(enrichments.columns))
+        for col in enrichments:
+            attr_nodes = safe.attributes[col]
+            attr_nodes = set(attr_nodes[attr_nodes.astype(bool)].index)
+            enr_nodes = set((enrichments[col][enrichments[col] > 0]).index)
+            
+            data2 = []
+            
+            for term, term_nodes in terms.iteritems():
+                k1 = len(enr_nodes.intersection(term_nodes).intersection(attr_nodes))
+                M1 = all_annotated
+                n1 = len(enr_nodes.intersection(attr_nodes))
+                N1 = len(term_nodes.intersection(attr_nodes))
+                
+                if k1:
+#                     hit_ratio1 = k1 * 100. / n1
+#                     function_ratio1 = N1 * 100. / all_annotated
+#                     fold1 = hit_ratio1 / function_ratio1
+                    fold1 = (k1 * all_annotated) / float(n1 * len(term_nodes)) # N1
+                    
+                    data2.append((
+                            term.alias, 
+                            hypergeom.pmf(k1, M1, n1, N1), 
+                            fold1,
+                            '%d / %d, %.1f%%' % (n1, len(attr_nodes), n1 * 100. / len(attr_nodes)),
+                            '%d / %d, %.1f%%' % (k1, n1, k1 * 100. / n1),
+                            '%d / %d, %.1f%%' % (len(term_nodes), all_annotated, len(term_nodes) * 100. / all_annotated),
+                            ','.join([node_map[n]['label'] for n in enr_nodes.intersection(term_nodes).intersection(attr_nodes)]),
+                        ))
+                
+                colnames = ['Term', 'p-value', 'fold change', 'Fraction of input gene list', 'Cluster frequency', 'Background frequency', 'Genes']
+                
+                p.DataFrame(data2, columns=colnames).sort_values('p-value').to_excel(res_data, index=None, sheet_name=col)
+        
+        res_data.save()
+        
+        output.seek(0)
         
         filename = 'tcm-safe-%s.xlsx' % (datetime.datetime.now().strftime('%y%m%d'), )
-        return xlsx_response(enrichments.reset_index().sort_values(enrichments.columns[0], ascending=False), filename, index=None)
+        resp = FileResponse(output, content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+        resp['Content-Disposition'] = 'attachment; filename="%s"' % (filename, )
+        return resp
     
     result = {}
     for col in enrichments:
