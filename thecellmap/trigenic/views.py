e8dc6c7c718f943ba306d0d600762fca9bc54ecb
matej <matej@7e663dc3-c743-4763-bac1-1f4d75230683>
2017-06-05 20:36:50 +0000

    - Update for trigenic data

--- thecellmap/trigenic/views.py
+++ thecellmap/trigenic/views.py
@@ -1,15 +1,17 @@
+import io
+
 from django.db.models import Q
-from django.http.response import JsonResponse
+from django.http.response import JsonResponse, FileResponse
 from django.views.decorators.http import require_GET
 
+from base.download import xlsx_response
 from base.utils import require_get_params, add_headers
-from trigenic.models import TriStrainSet, TriStrain
+import pandas as p
+
+from .models import TriStrainSet, TriStrain
 
 
-@require_GET
-@add_headers(**{'Access-Control-Allow-Origin': '*'})
-@require_get_params(params=['gene', 'score_type'])
-def scores(request):
+def _get_scores(request):
     gene = request.GET['gene']
     typ = request.GET['score_type']
     
@@ -19,9 +21,9 @@ def scores(request):
         strain_set = TriStrainSet.objects.filter(Q(double_mutant__gene1=gene) | Q(double_mutant__gene2=gene))
         
         if strain_set.count() == 0:
-            return JsonResponse({'error': 'Selected gene was not screened as a query'})
+            return {'error': 'Selected gene was not screened as a query'}
         elif strain_set.count() > 1:
-            return JsonResponse({'error': 'Selected gene returned more than one double mutant'})
+            return {'error': 'Selected gene returned more than one double mutant'}
         
         strain_set = strain_set.select_related()[0]
         
@@ -39,12 +41,78 @@ def scores(request):
         strain = TriStrain.objects.filter((Q(gene1=gene) | Q(gene2=gene)), is_query=False)
         
         if strain.count() == 0:
-            return JsonResponse({'error': 'Selected gene was not screened as an array'})
+            return {'error': 'Selected gene was not screened as an array'}
         
         strain = strain[0]
         scores = strain.get_array_scores(Q(query__is_double_mutant=True, score__lt=0) | Q(query__is_double_mutant=False), pvalue__lt=0.05)
         
         for g, s, p in scores.itertuples(index=False):
             result.setdefault('a', {'strain': strain.pk, 'scores': []})['scores'].append((g,s,p))
+    
+    return result
+
+def list_to_df(scores, strains, short=False):
+    df = p.DataFrame(scores, columns=['g', 'Score', 'p-value'])
+    
+    index = []
+    for g in df['g']:
+        s = strains[g]
+        row = [s.gene1.orf, s.gene1.name, '', '', s.allele, s.boonelab_id, s.is_double_mutant and 'Trigenic' or 'Digenic']
+        if s.is_double_mutant:
+            row[2] = s.gene2.orf
+            row[3] = s.gene2.name
         
-    return JsonResponse(result)
+        index.append(row)
+    
+    idx = p.MultiIndex.from_tuples(index, names=['ORF 1', 'Gene 1', 'ORF 2', 'Gene 2', 'Allele', 'Strain ID', 'Interaction Type'])
+    if short:
+        idx = idx.droplevel(['ORF 2', 'Gene 2'])
+    
+    df.index = idx
+    
+    return df
+
+def download(request):
+    scores = _get_scores(request)
+    
+    if request.GET.get('score_type') == 'query':
+        strains = [scores[k]['strain'] for k in scores]
+        for k in scores:
+            strains += [g for g, _, _ in scores[k]['scores']]
+        strains = {s.pk: s for s in TriStrain.objects.filter(pk__in=strains).select_related('gene1', 'gene2')}
+        
+        output = io.BytesIO()
+        w = p.ExcelWriter(output, engine='xlsxwriter')
+
+        for k in scores:
+            df = list_to_df(scores[k]['scores'], strains, short=True)
+            df.to_excel(
+                w, 
+                sheet_name=strains[scores[k]['strain']].verbose_name(), 
+                columns=['Score', 'p-value'],)
+        
+        w.save()
+        
+        output.seek(0)
+        
+        resp = FileResponse(output, content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+        resp['Content-Disposition'] = 'attachment; filename="%s"' % ('kuzmin2017_trigenic_scores.xlsx', )
+        return resp
+    else:
+        strains = {s.pk: s for s in TriStrain.objects.filter(pk__in=[g for g,_,_ in scores['a']['scores']] + [scores['a']['strain']]).select_related('gene1', 'gene2')}
+        df = list_to_df(scores['a']['scores'], strains)
+        
+        return xlsx_response(
+            df, 
+            'kuzmin2017_trigenic_scores.xlsx', 
+            sheet_name=strains[scores['a']['strain']].verbose_name(),
+            columns=['Score', 'p-value'],
+        )
+
+@require_GET
+@add_headers(**{'Access-Control-Allow-Origin': '*'})
+@require_get_params(params=['gene', 'score_type'])
+def scores(request):
+    if 'download' in request.GET:
+        return download(request)
+    return JsonResponse(_get_scores(request))
